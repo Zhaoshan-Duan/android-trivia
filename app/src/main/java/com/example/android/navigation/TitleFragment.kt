3d81289a469455d79d795d413f0d8ad27ff8c108
Zhaoshan-Duan <joshua.duan47@gmail.com>
2022-11-10 15:52:21 -0800

    implemented About menu on titleFragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -29,14 +28,26 @@ class TitleFragment : Fragment() {
 
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+
         )
-//
 //                view: View ->
 //
 //            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
 //            // Using ktx
 //            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
 //        }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // if navigation UI dosen't handle the selection, we call super to let the app directly handle the menu item without navigating
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
