6de285a9ad7ec8dafc9763f6a8087bd2d1bd9c24
Zhaoshan-Duan <joshua.duan47@gmail.com>
2022-11-13 15:31:20 -0800

    implement sharing menu on winning screen

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,12 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log.i
+import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -28,18 +29,74 @@ import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false)
 
-        binding.nextMatchButton.setOnClickListener{ view: View ->
-            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         // get the arguments from the argument bundle
-        var args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_SHORT).show()
+
+//        Toast.makeText(context,
+//            "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+//            Toast.LENGTH_SHORT).show()
+
+        // show the winner menu
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    // inflate the menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+            // hide the share menu item if doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    // create share intent
+    private fun getShareIntent(): Intent {
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+
+        // Using ShareCompat API
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setType("text/plain")
+            .intent
+
+        // altenrative way to share
+//        val shareIntent =
+//            Intent(Intent.ACTION_SEND) // tells Android that we want activities that are registered with an intent filter to handle send action
+//        shareIntent.setType("text/plain"). // set the type of data we are sharing to text
+//        putExtra(Intent.EXTRA_TEXT, // key-value data structure
+//            getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+//        return shareIntent
+    }
+
+    // sharing
+    private fun shareSuccess() {
+        // start the activity with new intent
+        // will pop up a chooser since many activities have intent filters that can handle send text intent
+        startActivity(getShareIntent())
+    }
+
+    // connect to the menu action
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // navigate to another app using custom intent
+        when (item.itemId) {
+            // when the id matches in the menu xml
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
