11363a2b31f2a568afa5bc9f63c516b65a706e6b
Zhaoshan-Duan <joshua.duan47@gmail.com>
2024-06-18 17:58:12 -0700

    add title fragment to main actvity; add style files

--- app/src/main/java/org/jojo/trivia/MainActivity.kt
+++ app/src/main/java/org/jojo/trivia/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import org.jojo.trivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
