a6e26b21c064c05ac24bbef962349e080ce5ee97
Zhaoshan-Duan <joshua.duan47@gmail.com>
2024-06-18 19:28:30 -0700

    add navigation gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,13 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
